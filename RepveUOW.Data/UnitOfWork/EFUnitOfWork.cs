86ea1956d282ac25d7b87cd355fbee7169065e95
Erkan Güzelküçük <guzelkucukerkan@gmail.com>
2016-06-27 18:58:50 +0300

    Project First Commit

---
+++ RepveUOW.Data/UnitOfWork/EFUnitOfWork.cs
@@ -0,0 +1,74 @@
+﻿using RepveUOW.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepveUOW.Data.UnitOfWork
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly EFBlogContext _dbContext;
+
+        public EFUnitOfWork(EFBlogContext dbContext)
+        {
+            Database.SetInitializer<EFBlogContext>(null);
+
+            if (dbContext == null)
+                throw new ArgumentNullException("dbContext can not be null.");
+
+            _dbContext = dbContext;
+
+            // Buradan istediğiniz gibi EntityFramework'ü konfigure edebilirsiniz.
+            //_dbContext.Configuration.LazyLoadingEnabled = false;
+            //_dbContext.Configuration.ValidateOnSaveEnabled = false;
+            //_dbContext.Configuration.ProxyCreationEnabled = false;
+        }
+
+        #region IUnitOfWork Members
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            return new EFRepository<T>(_dbContext);
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                // Transaction işlemleri burada ele alınabilir veya Identity Map kurumsal tasarım kalıbı kullanılarak
+                // sadece değişen alanları güncellemeyide sağlayabiliriz.
+                return _dbContext.SaveChanges();
+            }
+            catch
+            {
+                // Burada DbEntityValidationException hatalarını handle edebiliriz.
+                throw;
+            }
+        }
+        #endregion
+
+        #region IDisposable Members
+        // Burada IUnitOfWork arayüzüne implemente ettiğimiz IDisposable arayüzünün Dispose Patternini implemente ediyoruz.
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
